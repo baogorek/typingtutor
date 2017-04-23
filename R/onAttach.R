d8e3e8347afac55b1ea603b9680d8842e9ce7906
Ben Ogorek <baogorek@gmail.com>
2017-04-22 19:52:16 -0500

    explicitly listing crayon dependency in onAttach.R

--- R/onAttach.R
+++ R/onAttach.R
@@ -5,8 +5,8 @@
 .onAttach <- function(libname, pkgname) {
   if (interactive()) {
     packageStartupMessage("run the command `type_github()` to start typing!")
-    if (!has_color()){
-      packageStartupMessage(paste("ANSI color support not available. Windows",
+    if (!crayon::has_color()){
+      packageStartupMessage(paste("\nANSI color support is not available. Windows",
                                   "users\n may wish to try the ConEmu",
                                   "terminal emulator"))
     }
