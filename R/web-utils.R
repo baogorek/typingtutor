841e9254524a75de7346af27f366ba4100e9942c
Ben Ogorek <baogorek@gmail.com>
2017-05-16 18:47:25 -0500

    Adding httr functionality to read and write from firebase

--- R/web-utils.R
+++ R/web-utils.R
@@ -1,7 +1,29 @@
 
 grab_web_contents <- function() {
-  rvest::read_html(
-    "https://baogorek.github.io/typingtutor/site/signed-in.html"
-)}
+  browseURL("https://baogorek.github.io/typingtutor/site/signed-in.html")
+  user_input <- readline("Paste token from browser and press enter:")
+  firebase_metadata <- jsonlite::fromJSON(user_input)
+  
+
+  # Example of querying data:
+  data_url <- "https://typingtutor-9f7e9.firebaseio.com/with_chris.json"
+                    "?auth=", firebase_metadata$token)
+
+  read_request <- httr::GET(data_url,
+                            query = list(auth = firebase_metadata$token))
+  query_data <- httr::content(read_request)
+
+  # Example of writing data
+  write_url <- paste0("https://typingtutor-9f7e9.firebaseio.com/user_info.json",
+                    "?auth=", firebase_metadata$token)
+
+  write_request <- httr::PUT(write_url,
+                 body = list(d = list(inner_obj = list(four = "Jack", five = "Sparrow"))),
+                 encode = "json",
+                 verbose())
+
+  content(write_request)
+
+}
 
 
