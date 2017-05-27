34cbf7ee37d7abd664d9a6ca76467f10efd260db
Ben Ogorek <baogorek@gmail.com>
2017-05-27 17:38:36 -0500

    firebase functions for reading and writing data are complete

--- R/web-utils.R
+++ R/web-utils.R
@@ -1,5 +1,5 @@
 
-get_token <- function() {
+init <- function() {
   browseURL("https://baogorek.github.io/typingtutor/site/signed-in.html")
   refresh_token()
 }
@@ -21,26 +21,37 @@ refresh_token <- function() {
   attach(firebase_env)
 }
 
-do_things <- function() {
+write_data_to_firebase <- function(data_in_list) {
 
-  token <- as.environment("firebase_env")$token
-  userid <- as.environment("firebase_env")$userid
- 
-  ensure_user_exists_in_db(userid, token)
+  ensure_user_exists_in_db(as.environment("firebase_env")$userid,
+                           as.environment("firebase_env")$token)
 
   write_url <- paste0("https://typingtutor-9f7e9.firebaseio.com/user_info/",
-                      userid, ".json?auth=", token)
+                      as.environment("firebase_env")$userid, ".json?auth=",
+                      as.environment("firebase_env")$token)
+
   msec_since_1970 <- as.character(round(unclass(Sys.time()) * 1000))
   timestamp_posix <- paste0("p", msec_since_1970) 
-  data_to_write <- list(this_timestamp = list(four = "Jack", five = "Sparrow"))
+  data_to_write <- list(this_timestamp = data_in_list)
   names(data_to_write) <- timestamp_posix
 
-  write_request <- httr::PUT(write_url,
-                 body = data_to_write,
-                 encode = "json")
+  write_response <- httr::PATCH(write_url, body = data_to_write,
+                                encode = "json")
 
-  httr::content(write_request)
+  handle_response(write_response) 
 
+}
+
+handle_response <- function(httr_response) {
+
+  read_error_message <- ifelse("error" %in% names(httr_response),
+                               query_data$error, "")
+  if (read_error_message == "Auth token is expired") {
+    cat("Auth token is expired. Please enter token from refreshed page\n")
+    init() 
+  } else if (nchar(read_error_message) > 0) {
+    stop(read_error_message)
+  }
 }
 
 ensure_user_exists_in_db <- function(userid, token) {
@@ -49,9 +60,11 @@ ensure_user_exists_in_db <- function(userid, token) {
 
   read_request <- httr::GET(data_url, query = list(auth = token))
   query_data <- httr::content(read_request)
+  handle_response(query_data)
+
   if (is.null(query_data)) {
   
-    write_url <- paste0("https://typingtutor-9f7e9.firebaseio.com/user_info2",
+    write_url <- paste0("https://typingtutor-9f7e9.firebaseio.com/user_info",
                         ".json", "?auth=", token)
     data_to_write <- list(userid = "placeholder")
     names(data_to_write) <- userid
@@ -59,5 +72,6 @@ ensure_user_exists_in_db <- function(userid, token) {
     write_response <- httr::PUT(write_url,
                                 body = data_to_write,
                                 encode = "json")
+    print(write_response)
   }
 }
