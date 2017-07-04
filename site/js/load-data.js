2e5656c9a5fbb1ccba6a9e143799fbca8b5341ad
Ben Ogorek <baogorek@gmail.com>
2017-07-04 11:04:02 -0500

    Trying different way to deal with users w/o data

--- site/js/load-data.js
+++ site/js/load-data.js
@@ -18,16 +18,11 @@ initApp = function() {
       document.getElementById('welcome').textContent =
         'Welcome to typingtutor, ' + user.displayName + '!';
 
-      var dbRefObject = getDbRef(user);
-      if (dbRefObject) {
-        var typingData;
-        dbRefObject.on('value', fillInPage);
-      } else {
-        lastTryMsg = 'No data yet. Open R and play a round of typingtutor!'
-        document.getElementById('last-try').innerHTML = lastTryMsg;
-        document.getElementById('history').innerHTML = lastTryMsg;
-      }
-    } else { // user needs to sign in
+      var typingData; // for debugging help
+      var dbRefObject = firebase.database().ref();
+      dbRefObject.on('value', fillInPage);
+
+      } else { // user needs to sign in
       window.location = "https://baogorek.github.io/typingtutor/"
     }
   });
@@ -35,28 +30,34 @@ initApp = function() {
 
 function fillInPage(snapshot) {
 
-  typingData = snapshot.val();
+  typingData = snapshot.val()['user_info'][user.uid];
   
-  var timestamps = Object.keys(typingData).sort().reverse();
-  var last_obj = typingData[timestamps[0]];
-  var timeLastEx = new Date(parseInt(timestamps[0].substring(1)));
-  var lastTryMsg = 'You last practiced on repo <b>' +
-    typingData[timestamps[0]]['repo'] + '</b> with file <b>' +
-    typingData[timestamps[0]]['r_file'] + '</b> and expression group ' +
-    '<i>' + typingData[timestamps[0]]['expression_group'] + '</i>. ' +
-    'Your score was <b>' + Math.round(typingData[timestamps[0]]['wpm']) +
-    '</b> words per minute!';
-  
-  document.getElementById('last-try').innerHTML = lastTryMsg + '\n\n';
-  document.getElementById('last-try-d3').innerHTML = '';
-  
-  var exerciseInfo = transpose(typingData); 
-  
-  var fileData = exerciseInfo[last_obj.repo][last_obj.r_file];
-  var exerciseData = fileData[last_obj.expression_group];
-  exerciseData.sort(dateSort);
-  createBarChart(exerciseData);
-  
-  document.getElementById('history').innerHTML = "";
-  createHistoryList(exerciseInfo);
+  if (typingData) {
+    var timestamps = Object.keys(typingData).sort().reverse();
+    var last_obj = typingData[timestamps[0]];
+    var timeLastEx = new Date(parseInt(timestamps[0].substring(1)));
+    var lastTryMsg = 'You last practiced on repo <b>' +
+      typingData[timestamps[0]]['repo'] + '</b> with file <b>' +
+      typingData[timestamps[0]]['r_file'] + '</b> and expression group ' +
+      '<i>' + typingData[timestamps[0]]['expression_group'] + '</i>. ' +
+      'Your score was <b>' + Math.round(typingData[timestamps[0]]['wpm']) +
+      '</b> words per minute!';
+    
+    document.getElementById('last-try').innerHTML = lastTryMsg + '\n\n';
+    document.getElementById('last-try-d3').innerHTML = '';
+    
+    var exerciseInfo = transpose(typingData); 
+    
+    var fileData = exerciseInfo[last_obj.repo][last_obj.r_file];
+    var exerciseData = fileData[last_obj.expression_group];
+    exerciseData.sort(dateSort);
+    createBarChart(exerciseData);
+    
+    document.getElementById('history').innerHTML = "";
+    createHistoryList(exerciseInfo);
+  } else {
+    lastTryMsg = 'No data yet. Open R and play a round of typingtutor!'
+    document.getElementById('last-try').innerHTML = lastTryMsg;
+    document.getElementById('history').innerHTML = lastTryMsg;
+  }
 }
